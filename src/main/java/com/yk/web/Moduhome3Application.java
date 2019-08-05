de1564776e39e15a41f5195118124b429c200d9e
midas123 <midasyk123@naver.com>
2019-08-05 17:12:37 +0900

    프로젝트 폴더 생성

---
+++ src/main/java/com/yk/web/Moduhome3Application.java
@@ -0,0 +1,13 @@
+package com.yk.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Moduhome3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Moduhome3Application.class, args);
+	}
+
+}
